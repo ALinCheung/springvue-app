23f307aa1e553a4911243be3e8790dbd4d5bedd7
张文霖 <zhangwl@eastcom-sw.com>
2023-01-28 00:00:49 +0800

    添加 AGENTS.md 配置文档

--- springvue-ui/packages/common/components/layout/aside.vue
+++ springvue-ui/packages/common/components/layout/aside.vue
@@ -26,6 +26,10 @@ const menus: Array<MenuItem> = [
     path: "/uploadByPieces",
     title: "分片上传",
   },
+  {
+    path: "/puzzleVcode",
+    title: "验证码",
+  },
 ];
 const router = useRouter() as Router;
 const handleMenu = (menuItem: MenuItem) => {
